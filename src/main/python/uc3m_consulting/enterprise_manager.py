64c5729ddea0f0b2121757b5c0c31999c012ea7e
100524862 <100524862@alumnos.uc3m.es>
2026-03-05 11:29:20 +0100

    funcion register_project hecha

--- src/main/python/uc3m_consulting/enterprise_manager.py
+++ src/main/python/uc3m_consulting/enterprise_manager.py
@@ -1,9 +1,19 @@
 """Module """
+from src.main.python.uc3m_consulting import EnterpriseProject
+
 
 class EnterpriseManager:
     """Class for providing the methods for managing the orders"""
     def __init__(self):
         pass
+
+    def register_project(self, company_cif:str, project_acronym:str, project_description:str,
+                         date:str, department:str, budget:float)->str:
+        """Method for registering a new project"""
+        my_project = EnterpriseProject(company_cif=company_cif, project_budget=budget,
+                                       project_acronym=project_acronym, project_description=project_description,
+                                       starting_date=date, department=department)
+        return my_project.project_id
 
     @staticmethod
     def validate_cif(cif: str):
