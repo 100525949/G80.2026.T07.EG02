8cb8d40b54a2a1d96b1d15390a837ec15bcababe
100525949 <100525949@alumnos.uc3m.es>
2026-03-12 11:43:23 +0100

    enterprise manager actualizado y test copiado del commits anterior

--- src/main/python/uc3m_consulting/enterprise_manager.py
+++ src/main/python/uc3m_consulting/enterprise_manager.py
@@ -1,9 +1,20 @@
 """Module """
+import re
+from enterprise_management_exception import EnterpriseManagementException
 
 class EnterpriseManager:
     """Class for providing the methods for managing the orders"""
     def __init__(self):
         pass
+
+    def register_project(self, company_cif: str, project_acronym: str,
+                         project_description: str, date: str,
+                         department: str, budget: float) -> str:
+        self.validate_cif(company_cif)
+        my_project = EnterpriseProject(company_cif=company_cif, project_acronym=project_acronym,
+                                       department=department, project_budget=budget,
+                                       project_description=project_description, starting_date=date)
+        return my_project.project_id
 
     @staticmethod
     def validate_cif(cif: str):
--- src/unittest/python/test_register_project_tests.py
+++ src/unittest/python/test_register_project_tests.py
@@ -1,9 +1,34 @@
 """class for testing the regsiter_order method"""
 import unittest
+from datetime import date
+
 from uc3m_consulting import EnterpriseManager
+from uc3m_consulting import EnterpriseManagerException
 
 class MyTestCase(unittest.TestCase):
     """class for testing the register_order method"""
+    def test_valid_1( self ):
+        importe = 50000.00
+        my_manager = EnterpriseManager()
+        valor_devuelto = my_manager.register_project(company_cif=cif, project_acronym=acronym,
+                                                     project_description=description, date=date,
+                                                     department=department, budget=importe)
+        self.assertEqual(valor_devuelto, "84fb2cba79af99d1afee74619c")
+
+    def test_register_2( self ):
+        """Cif not valid"""
+        my_manager = EnterpriseManager()
+        cif = "A1234567"
+        acronym = "PEPESL"
+        description = "marketing de pepe"
+        department = "FINANCE"
+        date = "01/01/2025"
+        budget = 100000.00
+        with self.assertRaises(EnterpriseManagerException) as cm:
+            result = my_manager.register_project(company_cif=cif, project_acronym=acronym,
+                                                 project_description=description, date=date,
+                                                 department=department, budget=budget)
+        self.assertEqual(cm.exception.message, "ERROR: CIF not valid, length")
 
 
 if __name__ == '__main__':
